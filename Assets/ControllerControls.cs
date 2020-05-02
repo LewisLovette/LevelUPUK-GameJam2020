1738b79c5a6545041bb2842ffa13a109db80c60d
Lewis Lovette <lewislovette3@hotmail.co.uk>
2020-05-02 10:57:07 +0100

    📦 NEW: Shield, particles, shooting, map

--- Assets/ControllerControls.cs
+++ Assets/ControllerControls.cs
@@ -25,6 +25,38 @@ public class @ControllerControls : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""rLeft"",
+                    ""type"": ""Button"",
+                    ""id"": ""cd030852-e310-415e-bf16-7068c5acf31f"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""rRight"",
+                    ""type"": ""Button"",
+                    ""id"": ""1d58b69a-225f-4837-ad9c-f1aacd386c15"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""rUp"",
+                    ""type"": ""Button"",
+                    ""id"": ""71b42647-d4ab-4b55-82c0-51bb7a45e1d4"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""rDown"",
+                    ""type"": ""Button"",
+                    ""id"": ""ae610555-0ac8-4712-a531-b8528f141058"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -32,10 +64,54 @@ public class @ControllerControls : IInputActionCollection, IDisposable
                     ""name"": """",
                     ""id"": ""ead0bf62-613e-4b49-94e6-7a952363b9a1"",
                     ""path"": ""<Gamepad>/rightShoulder"",
-                    ""interactions"": ""Tap"",
+                    ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""bdf12748-29bf-4e2d-9663-3b4e3e9b7278"",
+                    ""path"": ""<XInputController>/rightStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""rLeft"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3cc552c8-6ffd-462e-bc6e-826e4faf6597"",
+                    ""path"": ""<XInputController>/rightStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""rRight"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b65053b6-de22-4f0f-b52b-9781a16a8c40"",
+                    ""path"": ""<XInputController>/rightStick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""rUp"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5a34ce4e-0474-4ede-92d4-d3a62eea83e2"",
+                    ""path"": ""<XInputController>/rightStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""rDown"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -53,6 +129,10 @@ public class @ControllerControls : IInputActionCollection, IDisposable
         // Main
         m_Main = asset.FindActionMap("Main", throwIfNotFound: true);
         m_Main_Fire = m_Main.FindAction("Fire", throwIfNotFound: true);
+        m_Main_rLeft = m_Main.FindAction("rLeft", throwIfNotFound: true);
+        m_Main_rRight = m_Main.FindAction("rRight", throwIfNotFound: true);
+        m_Main_rUp = m_Main.FindAction("rUp", throwIfNotFound: true);
+        m_Main_rDown = m_Main.FindAction("rDown", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -103,11 +183,19 @@ public class @ControllerControls : IInputActionCollection, IDisposable
     private readonly InputActionMap m_Main;
     private IMainActions m_MainActionsCallbackInterface;
     private readonly InputAction m_Main_Fire;
+    private readonly InputAction m_Main_rLeft;
+    private readonly InputAction m_Main_rRight;
+    private readonly InputAction m_Main_rUp;
+    private readonly InputAction m_Main_rDown;
     public struct MainActions
     {
         private @ControllerControls m_Wrapper;
         public MainActions(@ControllerControls wrapper) { m_Wrapper = wrapper; }
         public InputAction @Fire => m_Wrapper.m_Main_Fire;
+        public InputAction @rLeft => m_Wrapper.m_Main_rLeft;
+        public InputAction @rRight => m_Wrapper.m_Main_rRight;
+        public InputAction @rUp => m_Wrapper.m_Main_rUp;
+        public InputAction @rDown => m_Wrapper.m_Main_rDown;
         public InputActionMap Get() { return m_Wrapper.m_Main; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -120,6 +208,18 @@ public class @ControllerControls : IInputActionCollection, IDisposable
                 @Fire.started -= m_Wrapper.m_MainActionsCallbackInterface.OnFire;
                 @Fire.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnFire;
                 @Fire.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnFire;
+                @rLeft.started -= m_Wrapper.m_MainActionsCallbackInterface.OnRLeft;
+                @rLeft.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnRLeft;
+                @rLeft.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnRLeft;
+                @rRight.started -= m_Wrapper.m_MainActionsCallbackInterface.OnRRight;
+                @rRight.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnRRight;
+                @rRight.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnRRight;
+                @rUp.started -= m_Wrapper.m_MainActionsCallbackInterface.OnRUp;
+                @rUp.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnRUp;
+                @rUp.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnRUp;
+                @rDown.started -= m_Wrapper.m_MainActionsCallbackInterface.OnRDown;
+                @rDown.performed -= m_Wrapper.m_MainActionsCallbackInterface.OnRDown;
+                @rDown.canceled -= m_Wrapper.m_MainActionsCallbackInterface.OnRDown;
             }
             m_Wrapper.m_MainActionsCallbackInterface = instance;
             if (instance != null)
@@ -127,6 +227,18 @@ public class @ControllerControls : IInputActionCollection, IDisposable
                 @Fire.started += instance.OnFire;
                 @Fire.performed += instance.OnFire;
                 @Fire.canceled += instance.OnFire;
+                @rLeft.started += instance.OnRLeft;
+                @rLeft.performed += instance.OnRLeft;
+                @rLeft.canceled += instance.OnRLeft;
+                @rRight.started += instance.OnRRight;
+                @rRight.performed += instance.OnRRight;
+                @rRight.canceled += instance.OnRRight;
+                @rUp.started += instance.OnRUp;
+                @rUp.performed += instance.OnRUp;
+                @rUp.canceled += instance.OnRUp;
+                @rDown.started += instance.OnRDown;
+                @rDown.performed += instance.OnRDown;
+                @rDown.canceled += instance.OnRDown;
             }
         }
     }
@@ -143,5 +255,9 @@ public class @ControllerControls : IInputActionCollection, IDisposable
     public interface IMainActions
     {
         void OnFire(InputAction.CallbackContext context);
+        void OnRLeft(InputAction.CallbackContext context);
+        void OnRRight(InputAction.CallbackContext context);
+        void OnRUp(InputAction.CallbackContext context);
+        void OnRDown(InputAction.CallbackContext context);
     }
 }
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -13,16 +13,28 @@ public class Movement : MonoBehaviour
     private Vector3 moveDirection = Vector3.zero;
 
     private GameObject particles;
+    private GameObject shield;
     private ParticleSystem.EmissionModule fire;
     private ControllerControls controller;
 
-    //myGameObject.transform.localPosition = wallGameObject.GetComponent<PositionReferences>().GetNextPosition();
-
+    private Vector2 rotate;
+    private bool left = false;
+    private bool right = false;
     void Awake()
     {
         controller = new ControllerControls();
 
         controller.Main.Fire.performed += ctx => Shoot();
+        controller.Main.Fire.canceled += ctx => ShootStop();
+
+        controller.Main.rLeft.performed += ctx => RotateLeft();
+        controller.Main.rRight.performed += ctx => RotateRight();
+        controller.Main.rUp.performed += ctx => RotateUp();
+        controller.Main.rDown.performed += ctx => RotateDown();
+
+        controller.Main.rLeft.canceled += ctx => RotateStop();
+        controller.Main.rRight.canceled += ctx => RotateStop();
+
     }
 
     void Start()
@@ -30,11 +42,15 @@ public class Movement : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         particles = GameObject.Find("Particles");
         particles.transform.parent = transform;
+
+        shield = GameObject.Find("Shield");
+        shield.transform.parent = transform;
+
         fire = GameObject.Find("Particles").GetComponent<ParticleSystem>().emission;
         fire.rateOverTime = 0;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (characterController.isGrounded)
         {
@@ -44,10 +60,10 @@ public class Movement : MonoBehaviour
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
             moveDirection *= speed;
 
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
+            //if (Input.GetButton("Jump"))
+            //{
+            //    moveDirection.y = jumpSpeed;
+            //}
             if (Input.GetButtonDown("Fire1"))
             {
                 Shoot();
@@ -56,11 +72,6 @@ public class Movement : MonoBehaviour
             {
                 fire.rateOverTime = 0;
             }
-            else
-            {
-                fire.rateOverTime = 0;
-            }
-
         }
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
@@ -70,11 +81,53 @@ public class Movement : MonoBehaviour
 
         // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
+
+        if (left)
+        {
+            particles.transform.RotateAround(transform.position, Vector3.up, -300f * Time.deltaTime);
+            shield.transform.RotateAround(transform.position, Vector3.up, -300f * Time.deltaTime);
+        }
+        else if (right)
+        {
+            particles.transform.RotateAround(transform.position, Vector3.up, 300f * Time.deltaTime);
+            shield.transform.RotateAround(transform.position, Vector3.up, 300f * Time.deltaTime);
+        }
+        
     }
 
     void Shoot()
     {
         fire.rateOverTime = 10;
+    }
+
+    void RotateLeft()
+    {
+        left = true;
+        //particles.transform.RotateAround(transform.position, Vector3.up, 50f);
+    }
+    void RotateRight()
+    {
+        right = true;
+        //particles.transform.RotateAround(transform.position, Vector3.up, -50f);
+    }
+    void RotateUp()
+    {
+
+    }
+    void RotateDown()
+    {
+
+    }
+
+    void RotateStop()
+    {
+        left = false;
+        right = false;
+    }
+
+    void ShootStop()
+    {
+        fire.rateOverTime = 0;
     }
 
     private void OnEnable()
