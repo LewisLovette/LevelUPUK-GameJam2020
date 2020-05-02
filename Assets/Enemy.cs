122bd495dd3734aad6f0d7c7a28ad2e0a6a2d31e
Lewis Lovette <lewislovette3@hotmail.co.uk>
2020-05-02 20:33:26 +0100

    👌 IMPROVE: Rotato shield

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,16 +6,27 @@ public class Enemy : MonoBehaviour
 {
 
     GameObject player;
+    GameObject[] shields;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        shields = GameObject.FindGameObjectsWithTag("eShield");
+        foreach (var shield in shields)
+        {
+            shield.transform.parent = transform;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        foreach(var shield in shields)
+        {
+            shield.transform.RotateAround(transform.position, Vector3.up, 300f * Time.deltaTime);
+        }
+
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 0.1f);
         transform.LookAt(player.transform);
     }
