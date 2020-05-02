d81c7338594f40eaebfaeefea352adb18d25d7c8
Lewis Lovette <lewislovette3@hotmail.co.uk>
2020-05-02 18:14:33 +0100

    📦 NEW: v basic enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 0.1f);
+        transform.LookAt(player.transform);
+    }
+}
